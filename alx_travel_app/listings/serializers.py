76fd5958ca18e2f5268412036cacd5b29ebd8ea5
Elias <eliasgirma.h@gmail.com>
2025-06-01 21:48:18 +0300

    alx travel app

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from .models import Listing, Booking
 
+# Serializer for the Listing model
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
-        fields = '__all__'
+        fields = '__all__'  # Includes all fields from the model
 
+
+# Serializer for the Booking model
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = '__all__'
+        fields = '__all__'  # Includes all fields from the model
